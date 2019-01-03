0416d8ed55708d54eb4c80ca45ed8d2814e9908a
Cheshire Cat <thacheshirecat@gmail.com>
2019-01-03 08:37:39 -0800

    Format and add the first algorithm to the algorithms section.

--- src/app/algorithms/algorithms.component.ts
+++ src/app/algorithms/algorithms.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlgorithmsComponent implements OnInit {
 
+  addingonetonresult = "--";
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  addingOneToN(n: number)
+  {
+    if(n <= 0)
+    {
+      return 0;
+    }
+    return n + this.addingOneToN(n - 1);
+  }
+
+  initAddingOneToN(n: string)
+  {
+    let inputNumber = parseInt(n);
+    let result = this.addingOneToN(inputNumber);
+    this.addingonetonresult = "For " + n + " the answer is " + result;
+  }
 }
